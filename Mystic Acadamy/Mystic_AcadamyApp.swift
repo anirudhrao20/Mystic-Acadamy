91ef8d9f3d61ced327c3ff2e07da90898499848f
Anirudh Rao <anirudhrao20@gmail.com>
2023-03-03 09:53:31 -0800

    Initial Commit

---
+++ Mystic Acadamy/Mystic_AcadamyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mystic_AcadamyApp.swift
+//  Mystic Acadamy
+//
+//  Created by Anirudh Rao on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Mystic_AcadamyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
